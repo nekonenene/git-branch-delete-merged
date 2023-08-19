5ee1894d908bae271874c10ce3f5b3d1a55de5c3
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2023-08-20 04:18:46 +0900

    Output branch names

--- cmd/executor.go
+++ cmd/executor.go
@@ -4,16 +4,26 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 func Exec() {
 	ParseParameters()
 
-	output, err := exec.Command("git", "-v").CombinedOutput()
+	err := exec.Command("git", "-v").Run()
 	if err != nil {
 		log.Fatal("command not found: git")
 	}
 
-	fmt.Println("Hello!")
-	fmt.Printf("%s", output)
+	output, err := exec.Command("git", "for-each-ref", "refs/heads/", "--format=%(refname:short)").CombinedOutput()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outputString := strings.TrimSpace(string(output))
+	branchNames := strings.Split(outputString, "\n")
+
+	for _, branchName := range branchNames {
+		fmt.Println(branchName)
+	}
 }
