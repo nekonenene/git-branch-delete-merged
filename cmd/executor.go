c7f2d0034efc9b72ccc6bcc17b12b2ad538e6d9f
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2023-08-20 04:03:55 +0900

    Check git command exists

--- cmd/executor.go
+++ cmd/executor.go
@@ -2,10 +2,18 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
 )
 
 func Exec() {
 	ParseParameters()
 
+	output, err := exec.Command("git", "-v").CombinedOutput()
+	if err != nil {
+		log.Fatal("command not found: git")
+	}
+
 	fmt.Println("Hello!")
+	fmt.Printf("%s", output)
 }
